c54e0d812e2814b654c47513a9b5ae81c2533c97
Art <d.art.shishkin@gmail.com>
2021-06-11 18:12:11 +0300

    16. Generating PKCE Code Challenge (#1)

--- pkce/src/main/java/net/shyshkin/study/oauth/pkce/PkceUtil.java
+++ pkce/src/main/java/net/shyshkin/study/oauth/pkce/PkceUtil.java
@@ -1,5 +1,8 @@
 package net.shyshkin.study.oauth.pkce;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -13,6 +16,17 @@ public class PkceUtil {
                 .getUrlEncoder()
                 .withoutPadding()
                 .encodeToString(codeVerifier);
+    }
+
+    public String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+        var bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        var messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes);
+        byte[] digest = messageDigest.digest();
+        return Base64
+                .getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(digest);
     }
 
 }
