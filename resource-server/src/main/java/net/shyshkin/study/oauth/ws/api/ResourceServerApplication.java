afa4c0218a447af50ecddb999c7464152145375a
Art <d.art.shishkin@gmail.com>
2021-06-14 00:18:32 +0300

    38. Creating a new project (#3 Section 5: OAuth Resource Server)

---
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
---
+++ resource-server/src/test/java/net/shyshkin/study/oauth/ws/api/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.ws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
