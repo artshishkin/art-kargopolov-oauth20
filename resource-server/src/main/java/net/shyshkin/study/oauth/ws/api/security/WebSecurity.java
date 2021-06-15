8db67850025ac5cb4400d69ef0ce9d888db99848
Art <d.art.shishkin@gmail.com>
2021-06-15 21:25:11 +0300

    49. Configure Scope-based access control (#5)

--- resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.oauth.ws.api.security;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,7 +10,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeRequests((requests) -> requests
+                .antMatchers(HttpMethod.GET, "/users/**").hasAuthority("SCOPE_profile")
+                .anyRequest().authenticated());
         http.oauth2ResourceServer().jwt();
     }
 }
