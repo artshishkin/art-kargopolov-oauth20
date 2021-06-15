9535a349483f0ec186da7fb941444247ef773363
Art <d.art.shishkin@gmail.com>
2021-06-15 21:06:17 +0300

    47. Create WebSecurityConfigurerAdapter and enable Web Security (#5)

---
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.oauth.ws.api.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+
+
+
+}
