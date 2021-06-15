fe91fcb386093aad32a26dcc44390cda7b867efd
Art <d.art.shishkin@gmail.com>
2021-06-15 21:18:33 +0300

    48. Override the default HttpSecurity configuration (#5)

--- resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
@@ -1,12 +1,15 @@
 package net.shyshkin.study.oauth.ws.api.security;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-
-
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.oauth2ResourceServer().jwt();
+    }
 }
