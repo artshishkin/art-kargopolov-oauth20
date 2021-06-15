caed9d6bd77dccad9a1c6e038d802f2bb8386be2
Art <d.art.shishkin@gmail.com>
2021-06-15 17:22:42 +0300

    44. Accessing Principal and JWT Claims (#3)

---
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/controllers/TokenController.java
@@ -0,0 +1,20 @@
+package net.shyshkin.study.oauth.ws.api.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @GetMapping
+    public Map<String, Jwt> getToken(@AuthenticationPrincipal Jwt jwt) {
+        return Collections.singletonMap("principal", jwt);
+    }
+}
