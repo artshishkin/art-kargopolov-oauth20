b47e3266c2506f4fe8d50e7dd759782b86282549
Art <d.art.shishkin@gmail.com>
2021-06-16 12:19:05 +0300

    63. Enable Method Level Security (#7 Section 8: Resource Server: Method Level Security)

--- resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/security/WebSecurity.java
@@ -1,12 +1,14 @@
 package net.shyshkin.study.oauth.ws.api.security;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
