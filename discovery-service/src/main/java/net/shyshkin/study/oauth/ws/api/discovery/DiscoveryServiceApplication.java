9d7dd2ced8febaa1346965c3d56483e006c65eab
Art <d.art.shishkin@gmail.com>
2021-06-17 15:57:46 +0300

    80. Creating Eureka Discovery Service Project (#9 Section 10: Eureka Discovery Service)

---
+++ discovery-service/src/main/java/net/shyshkin/study/oauth/ws/api/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.ws.api.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
