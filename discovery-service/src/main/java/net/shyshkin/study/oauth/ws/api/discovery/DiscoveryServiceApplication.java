1805b69552999b5cf1daf1f52ede1f7888e087f6
Art <d.art.shishkin@gmail.com>
2021-06-17 16:12:21 +0300

    81. Configuring Eureka project (#9)

--- discovery-service/src/main/java/net/shyshkin/study/oauth/ws/api/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/net/shyshkin/study/oauth/ws/api/discovery/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package net.shyshkin.study.oauth.ws.api.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
