62c8e77bd5d45743a8487f0a6aa4d6194c2ff7b7
Art <d.art.shishkin@gmail.com>
2021-06-17 18:09:16 +0300

    85. Exercise - make services to be eureka clients in local profile (#10)

--- albums-service/src/main/java/net/shyshkin/study/oauth/ws/api/albums/AlbumsServiceApplication.java
+++ albums-service/src/main/java/net/shyshkin/study/oauth/ws/api/albums/AlbumsServiceApplication.java
@@ -2,7 +2,9 @@ package net.shyshkin.study.oauth.ws.api.albums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AlbumsServiceApplication {
 
