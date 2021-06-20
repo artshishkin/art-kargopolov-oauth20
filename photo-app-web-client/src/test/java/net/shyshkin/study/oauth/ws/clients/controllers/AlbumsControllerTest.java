61499e38534ba78a9eb2b173f0910f0d3557ae94
Art <d.art.shishkin@gmail.com>
2021-06-20 22:55:16 +0300

    modified WebMvcTest (#13)

--- photo-app-web-client/src/test/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsControllerTest.java
+++ photo-app-web-client/src/test/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsControllerTest.java
@@ -5,11 +5,13 @@ import net.shyshkin.study.oauth.ws.clients.services.AlbumsService;
 import net.shyshkin.study.oauth.ws.clients.services.HardcodedAlbumsService;
 import org.hamcrest.beans.HasPropertyWithValue;
 import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
@@ -27,6 +29,7 @@ class AlbumsControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @Disabled("Test broken because of included security")
     void getAllAlbums() throws Exception {
 
         //when
@@ -47,6 +50,23 @@ class AlbumsControllerTest {
                                 )
                         )
                 )
+        ;
+
+    }
+
+    @Test
+    void getAllAlbums_redirect() throws Exception {
+
+        //given
+        String redirectUrl = "http://localhost:8080/auth/realms/katarinazart/protocol/openid-connect/auth?response_type=code&client_id=photo-app-webclient&scope=openid%20profile%20roles&state=760bKRabxF2PV02_q5dxMPIwiP4fIq2cZ9Cbsf0txHU%3D&redirect_uri=http://localhost:8050/login/oauth2/code/photo-app-webclient&nonce=zaPtbH-AlgMzvbUHtnK_18J8Jx88FrS_4bLVzV7dTUg";
+
+        //when
+        mockMvc
+                .perform(get("/albums"))
+
+                //then
+                .andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlPattern("http://*/oauth2/authorization/photo-app-web-client"));
         ;
 
     }
