41d1dc9a599aff6d36c76c5fcd020f0eae6329ff
Art <d.art.shishkin@gmail.com>
2021-06-20 22:33:11 +0300

    104. Reading ID Token (#13)

--- photo-app-web-client/src/main/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsController.java
+++ photo-app-web-client/src/main/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsController.java
@@ -1,11 +1,15 @@
 package net.shyshkin.study.oauth.ws.clients.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.oauth.ws.clients.services.AlbumsService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class AlbumsController {
@@ -13,7 +17,14 @@ public class AlbumsController {
     private final AlbumsService albumsService;
 
     @GetMapping("/albums")
-    public String getAllAlbums(Model model) {
+    public String getAllAlbums(
+            Model model,
+            @AuthenticationPrincipal OidcUser principal) {
+
+        log.debug("OpenId Connect User principal: {}", principal);
+
+        String idTokenValue = principal.getIdToken().getTokenValue();
+        log.debug("OpenId Connect token: {}", idTokenValue);
 
         model.addAttribute(
                 "albums",
