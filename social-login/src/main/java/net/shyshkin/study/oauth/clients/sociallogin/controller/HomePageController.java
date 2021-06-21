7bca28ffc5f90f80e909cedabc82af3bf9bb79f2
Art <d.art.shishkin@gmail.com>
2021-06-21 16:27:56 +0300

    122. Trying how Facebook login works (#15)

--- social-login/src/main/java/net/shyshkin/study/oauth/clients/sociallogin/controller/HomePageController.java
+++ social-login/src/main/java/net/shyshkin/study/oauth/clients/sociallogin/controller/HomePageController.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.oauth.clients.sociallogin.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class HomePageController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String getHomePage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+
+        log.debug("OAuth2User: {}", principal);
 
         Optional.ofNullable(principal)
                 .map(OAuth2User::getName)
                 .ifPresent(name -> model.addAttribute("name", name));
+
+        Optional.ofNullable(principal)
+                .map(oAuth2User -> oAuth2User.getAttribute("name"))
+                .ifPresent(name -> model.addAttribute("nameAttribute", name));
         return "home";
     }
 
