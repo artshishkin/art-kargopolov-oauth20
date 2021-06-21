5c33e37be65fe2f1760081314859ff8bd57798a4
Art <d.art.shishkin@gmail.com>
2021-06-21 12:08:19 +0300

    106. HTTP Request to an Internal Resource Server (#13)

--- photo-app-web-client/src/main/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsController.java
+++ photo-app-web-client/src/main/java/net/shyshkin/study/oauth/ws/clients/controllers/AlbumsController.java
@@ -3,12 +3,7 @@ package net.shyshkin.study.oauth.ws.clients.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.oauth.ws.clients.services.AlbumsService;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AlbumsController {
 
     private final AlbumsService albumsService;
-    private final OAuth2AuthorizedClientService oauth2ClientService;
 
     @GetMapping("/albums")
     public String getAllAlbums(
@@ -36,17 +30,6 @@ public class AlbumsController {
                 "albums",
                 albumsService.getAllAlbums()
         );
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-
-        OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(
-                oauthToken.getAuthorizedClientRegistrationId(),
-                oauthToken.getName()
-        );
-
-        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
-        log.debug("JWT Access Token: {}", jwtAccessToken);
 
         return "albums";
     }
---
+++ photo-app-web-client/src/main/java/net/shyshkin/study/oauth/ws/clients/services/RestTemplateAlbumsService.java
@@ -0,0 +1,71 @@
+package net.shyshkin.study.oauth.ws.clients.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.oauth.ws.clients.dto.AlbumDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Service
+@Primary
+@RequiredArgsConstructor
+public class RestTemplateAlbumsService implements AlbumsService {
+
+    private final OAuth2AuthorizedClientService oauth2ClientService;
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private RestTemplate restTemplate;
+    private static final ParameterizedTypeReference<List<AlbumDto>> ALBUMS_DTO_LIST_TYPE = new ParameterizedTypeReference<>() {
+    };
+
+    @Value("${app.gateway.uri}")
+    private String gatewayUri;
+
+    @PostConstruct
+    void init() {
+        restTemplate = restTemplateBuilder
+                .rootUri(gatewayUri + "/albums")
+                .build();
+    }
+
+    @Override
+    public List<AlbumDto> getAllAlbums() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName()
+        );
+
+        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+        log.debug("JWT Access Token: {}", jwtAccessToken);
+
+        var requestEntity = RequestEntity
+                .get("/")
+                .headers(headers -> headers.setBearerAuth(jwtAccessToken))
+                .build();
+
+        ResponseEntity<List<AlbumDto>> responseEntity = restTemplate.exchange(requestEntity, ALBUMS_DTO_LIST_TYPE);
+
+        return responseEntity.getStatusCode() == HttpStatus.OK ? responseEntity.getBody() : Collections.emptyList();
+    }
+}
