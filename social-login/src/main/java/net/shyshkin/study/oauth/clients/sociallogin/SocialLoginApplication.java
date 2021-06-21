3665a7e15ffd505d7f7c19df852600b000f9243c
Art <d.art.shishkin@gmail.com>
2021-06-21 14:08:25 +0300

    115. Creating a new project (#15 Section 14: OAuth 2 - Social Login)

---
+++ social-login/src/main/java/net/shyshkin/study/oauth/clients/sociallogin/SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.clients.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginApplication.class, args);
+    }
+
+}
---
+++ social-login/src/test/java/net/shyshkin/study/oauth/clients/sociallogin/SocialLoginApplicationTests.java
@@ -0,0 +1,18 @@
+package net.shyshkin.study.oauth.clients.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
