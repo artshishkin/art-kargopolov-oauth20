1a66caff8e9aa3f1a6d817e5938b7bd0370cfebb
Art <d.art.shishkin@gmail.com>
2021-06-22 22:04:13 +0300

    136. Creating a new Spring Boot client application (#16)

---
+++ spa-javascript-client/src/main/java/net/shyshkin/study/oauth/clients/pkce/SpaJavascriptClientApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.clients.pkce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaJavascriptClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaJavascriptClientApplication.class, args);
+    }
+
+}
---
+++ spa-javascript-client/src/test/java/net/shyshkin/study/oauth/clients/pkce/SpaJavascriptClientApplicationTests.java
@@ -0,0 +1,18 @@
+package net.shyshkin.study.oauth.clients.pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaJavascriptClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
