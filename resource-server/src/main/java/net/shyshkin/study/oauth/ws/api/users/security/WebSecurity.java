7ea8c56f734dcdb51e635cf49b8b2dec42dbac7e
Art <d.art.shishkin@gmail.com>
2021-06-23 12:35:41 +0300

    150.1. Configure CORS in Resource Server - request though Gateway broken (#16)

--- resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/users/security/WebSecurity.java
+++ resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/users/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package net.shyshkin.study.oauth.ws.api.users.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -13,15 +19,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests
-                .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/info").permitAll()
-                .antMatchers(HttpMethod.GET, "/users/status/check").authenticated()
-                .antMatchers(HttpMethod.GET, "/users/scope/**").hasAuthority("SCOPE_profile")
-                .antMatchers(HttpMethod.GET, "/users/role/developer/**").hasRole("developer") //.hasAnyRole("developer","user")
-                .antMatchers(HttpMethod.GET, "/users/role/admin/**").hasRole("admin")
-                .antMatchers(HttpMethod.GET, "/users/role/no_developer/**").not().hasRole("developer")
-                .anyRequest().authenticated());
+
+        http.cors()
+                .and()
+                .authorizeRequests((requests) -> requests
+                        .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+                        .antMatchers(HttpMethod.GET, "/actuator/info").permitAll()
+                        .antMatchers(HttpMethod.GET, "/users/status/check").authenticated()
+                        .antMatchers(HttpMethod.GET, "/users/scope/**").hasAuthority("SCOPE_profile")
+                        .antMatchers(HttpMethod.GET, "/users/role/developer/**").hasRole("developer") //.hasAnyRole("developer","user")
+                        .antMatchers(HttpMethod.GET, "/users/role/admin/**").hasRole("admin")
+                        .antMatchers(HttpMethod.GET, "/users/role/no_developer/**").not().hasRole("developer")
+                        .anyRequest().authenticated());
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakAuthorityConverter());
@@ -29,5 +38,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+//        corsConfiguration.setAllowedMethods(List.of("POST"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+
+        var configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return configurationSource;
     }
 }
