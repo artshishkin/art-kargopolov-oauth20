5cf2f547830f9559175613e7f279ae2a0ad84037
Art <d.art.shishkin@gmail.com>
2021-06-23 20:23:27 +0300

    150.6. Testing PKCE - generate State, Code Verifier and Challenge (#16)

---
+++ spa-javascript-client/src/test/java/net/shyshkin/study/oauth/clients/pkce/SpaJavascriptClientApplicationIT.java
@@ -0,0 +1,239 @@
+package net.shyshkin.study.oauth.clients.pkce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.function.Executable;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@TestPropertySource(properties = {
+        "logging.level.net.shyshkin=debug",
+        "app.oauth.uri=http://${OAUTH_HOST}:${OAUTH_PORT}"
+})
+@ActiveProfiles("keycloak")
+@ContextConfiguration(initializers = SpaJavascriptClientApplicationIT.Initializer.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SpaJavascriptClientApplicationIT {
+
+    public static final String RESOURCE_OWNER_USERNAME = "shyshkin.art";
+    public static final String RESOURCE_OWNER_PASSWORD = "password_art_1";
+
+    static Network network = Network.newNetwork();
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>("postgres")
+            .withDatabaseName("keycloak")
+            .withUsername("keycloak")
+            .withPassword("password")
+            .withNetwork(network)
+            .withNetworkAliases("postgres");
+
+    @Container
+    static GenericContainer<?> keycloak = new GenericContainer<>("quay.io/keycloak/keycloak:latest")
+            .withNetwork(network)
+            .withNetworkAliases("keycloak")
+            .withEnv(Map.of(
+                    "DB_VENDOR", "POSTGRES",
+                    "DB_ADDR", "postgres",
+                    "DB_DATABASE", "keycloak",
+                    "DB_USER", "keycloak",
+                    "DB_SCHEMA", "public",
+                    "DB_PASSWORD", "password",
+                    "KEYCLOAK_USER", "admin",
+                    "KEYCLOAK_PASSWORD", "Pa55w0rd"
+            ))
+            .withCommand(
+                    "-b 0.0.0.0",
+                    "-Dkeycloak.migration.action=import",
+                    "-Dkeycloak.migration.provider=singleFile",
+                    "-Dkeycloak.migration.file=/tmp/export/realm-export.json",
+                    "-Dkeycloak.migration.strategy=IGNORE_EXISTING"
+            )
+            .withFileSystemBind("C:\\Users\\Admin\\IdeaProjects\\Study\\SergeyKargopolov\\OAuth20\\art-kargopolov-oauth20\\docker-compose\\keycloak-postgres\\export\\realm-export.json", "/tmp/export/realm-export.json")
+            .withExposedPorts(8080)
+            .dependsOn(postgreSQL)
+            .waitingFor(Wait.forLogMessage(".*Admin console listening on.*\\n", 1));
+
+    @Container
+    static GenericContainer<?> discoveryService = new GenericContainer<>("artarkatesoft/oauth20-discovery-service")
+            .withNetwork(network)
+            .withNetworkAliases("discovery-service")
+            .withExposedPorts(8080)
+            .waitingFor(Wait.forHealthcheck());
+
+    @Container
+    static GenericContainer<?> usersService = new GenericContainer<>("artarkatesoft/oauth20-resource-server")
+            .withNetwork(network)
+            .withNetworkAliases("users-service")
+            .withExposedPorts(8080)
+            .withEnv("eureka.client.enabled", "true")
+            .dependsOn(keycloak, discoveryService)
+            .waitingFor(Wait.forHealthcheck());
+
+    @Container
+    static GenericContainer<?> gatewayService = new GenericContainer<>("artarkatesoft/oauth20-api-gateway")
+            .withNetwork(network)
+            .withNetworkAliases("gateway-service")
+            .withExposedPorts(8080)
+            .withEnv("eureka.client.enabled", "true")
+            .withEnv("spring.profiles.active", "local")
+            .withEnv("discovery.service.uri", "http://discovery-service:8080")
+            .withEnv("eureka.client.registry-fetch-interval-seconds", "1")
+            .dependsOn(usersService, discoveryService)
+            .waitingFor(Wait.forHealthcheck());
+
+    @Container
+    static BrowserWebDriverContainer<?> browser = new BrowserWebDriverContainer<>()
+            .withCapabilities(new FirefoxOptions())
+            .withNetwork(network)
+            .withNetworkAliases("browser")
+            .withLogConsumer(new Slf4jLogConsumer(log))
+            .dependsOn(keycloak);
+
+    @Container
+    static GenericContainer<?> spaJavascriptClient = new GenericContainer<>("artarkatesoft/oauth20-spa-javascript-client")
+            .withNetwork(network)
+            .withNetworkAliases("spa-javascript-client")
+            .withEnv(Map.of(
+                    "logging.level.net.shyshkin", "debug",
+                    "app.redirect.host.uri", "http://spa-javascript-client:8080",
+                    "app.oauth.uri", "http://keycloak:8080",
+                    "app.gateway.uri", "http://gateway-service:8080",
+                    "app.users-api.uri", "http://users-service:8080"
+            ))
+            .withExposedPorts(8080)
+            .dependsOn(keycloak, gatewayService, usersService)
+            .withLogConsumer(new Slf4jLogConsumer(log))
+            .waitingFor(Wait.forLogMessage(".*Completed initialization in.*\\n", 1));
+
+    RemoteWebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = browser.getWebDriver();
+    }
+
+    @Test
+    void fullWorkflowTest() {
+        //given
+        String indexPageUrl = "http://spa-javascript-client:8080";
+
+        //visit index page should show correct content
+        driver.get(indexPageUrl);
+
+        assertThat(driver.getTitle()).isEqualTo("Javascript Application with PKCE");
+        assertThat(driver.findElementById("redirectHostUri").getText()).isEqualTo("http://spa-javascript-client:8080");
+        assertThat(driver.findElementById("oAuthServerUri").getText()).isEqualTo("http://keycloak:8080");
+        assertThat(driver.findElementById("usersApiUri").getText()).isEqualTo("http://users-service:8080");
+        assertThat(driver.findElementById("gatewayUri").getText()).isEqualTo("http://gateway-service:8080");
+        assertThat(driver.findElementById("resourceServerUri").getAttribute("value")).isEqualTo("http://users-service:8080");
+
+        //click on button `Generate Random State Value` should change text in `stateValue` field
+        driver.findElementById("generateStateBtn").click();
+        assertThat(driver.findElementById("stateValue").getText()).isNotEqualTo("Some Value");
+
+        //click on button `Generate Code Verifier Value` should change text in `codeVerifierValue` field
+        driver.findElementById("generateCodeVerifierBtn").click();
+        assertThat(driver.findElementById("codeVerifierValue").getText()).isNotEqualTo("Code Verifier Value");
+
+//        logCurrentPage();
+
+        //click on button `Generate Code Challenge Value` should change text in `codeVerifierValue` field
+        WebElement generateCodeChallengeBtn = driver.findElementById("generateCodeChallengeBtn");
+        generateCodeChallengeBtn.click();
+        log.debug("generateCodeChallengeBtn: {}", generateCodeChallengeBtn);
+
+        waitFor("generating code Challenge",
+                List.of(
+                        () -> driver.findElementById("generateCodeChallengeBtn").click(),
+                        () -> assertThat(driver.findElementById("codeChallengeValue").getText()).isNotEqualTo("Code Challenge Value")
+                ));
+    }
+
+    private void waitFor(String message, List<Executable> executableList) {
+
+        AtomicReference<String> lastUrl = new AtomicReference<>("");
+        AtomicReference<String> lastPageContent = new AtomicReference<>("");
+
+        AtomicLong start = new AtomicLong(System.currentTimeMillis());
+
+        await()
+                .timeout(5, TimeUnit.SECONDS)
+                .pollInterval(500, TimeUnit.MILLISECONDS)
+                .untilAsserted(() -> {
+
+                    String currentUrl = driver.getCurrentUrl();
+                    String pageSource = driver.getPageSource();
+
+                    if (Objects.equals(currentUrl, lastUrl.get()) && Objects.equals(pageSource, lastPageContent.get())) {
+
+                        log.debug("Waiting for {}... {} ms", message, System.currentTimeMillis() - start.get());
+                    } else {
+                        start.set(System.currentTimeMillis());
+                        log.debug("Current URL: {}", currentUrl);
+                        log.debug("Page: \n{}", pageSource);
+                        lastUrl.set(currentUrl);
+                        lastPageContent.set(pageSource);
+                    }
+                    assertAll(executableList);
+                });
+    }
+
+    private void signIn(String username, String password) {
+        //id = "username"
+        WebElement usernameField = driver.findElementById("username");
+        usernameField.sendKeys(username);
+
+        //id = "password"
+        WebElement passwordField = driver.findElementById("password");
+        passwordField.sendKeys(password);
+        passwordField.submit();
+    }
+
+    private void logCurrentPage() {
+        log.debug("Current URL: {}", driver.getCurrentUrl());
+        log.debug("Page: \n{}", driver.getPageSource());
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            System.setProperty("OAUTH_HOST", keycloak.getHost());
+            Integer keycloakPort = keycloak.getMappedPort(8080);
+            System.setProperty("OAUTH_PORT", String.valueOf(keycloakPort));
+        }
+    }
+}
