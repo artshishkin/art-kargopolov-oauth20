c7338082ef5a0b701efe79b30619f71f8b20ef36
Art <d.art.shishkin@gmail.com>
2021-06-24 18:09:12 +0300

    154. Creating RemoteUserStorageProviderFactory class (#17)

---
+++ user-storage-provider/src/main/java/net/shyshkin/study/oauth/spi/RemoteUserStorageProviderFactory.java
@@ -0,0 +1,20 @@
+package net.shyshkin.study.oauth.spi;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.storage.UserStorageProviderFactory;
+
+public class RemoteUserStorageProviderFactory implements UserStorageProviderFactory<RemoteUserStorageProvider> {
+
+    public static final String PROVIDER_NAME = "my-remote-mysql-user-storage-provider";
+
+    @Override
+    public RemoteUserStorageProvider create(KeycloakSession session, ComponentModel model) {
+        return new RemoteUserStorageProvider(session, model);
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_NAME;
+    }
+}
