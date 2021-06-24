a5642ed63026124c047328efa00573f42a141e8d
Art <d.art.shishkin@gmail.com>
2021-06-24 17:53:04 +0300

    152. Creating a new project (#17 Section 16: Keycloak Remote User Authentication. User Storage SPI.)

---
+++ user-storage-provider/src/main/java/net/shyshkin/study/oauth/spi/UserStorageProviderApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStorageProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserStorageProviderApplication.class, args);
+    }
+
+}
