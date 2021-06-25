f84abb9d7c9599d8edf0051dc7bf67a98acd4811
Art <d.art.shishkin@gmail.com>
2021-06-25 11:19:11 +0300

    161. Adding the verifyUserPassword to UsersApiService (#17)

--- user-storage-provider/src/main/java/net/shyshkin/study/oauth/spi/UsersApiService.java
+++ user-storage-provider/src/main/java/net/shyshkin/study/oauth/spi/UsersApiService.java
@@ -1,18 +1,20 @@
 package net.shyshkin.study.oauth.spi;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
 public interface UsersApiService {
 
     @GET
     @Path("/{userName}")
     User getUserDetails(@PathParam("userName") String userName);
 
+    @POST
+    @Path("/{userName}/verify-password")
+    @Produces(MediaType.APPLICATION_JSON)
+    VerifyPasswordResponse verifyUserPassword(
+            @PathParam("userName") String userName,
+            String password);
 }
---
+++ user-storage-provider/src/main/java/net/shyshkin/study/oauth/spi/VerifyPasswordResponse.java
@@ -0,0 +1,15 @@
+package net.shyshkin.study.oauth.spi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyPasswordResponse {
+
+    private boolean result;
+
+}
+
