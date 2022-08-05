4e271dfa2df32b3ed5a57c09c9bbb893fd2502cd
Art <d.art.shishkin@gmail.com>
2022-08-05 16:06:46 +0300

    171. Creating a new project (#32 Section 17: The New Spring Authorization Server)

---
+++ new-spring-authorization-server/src/main/java/net/shyshkin/study/oauth/spring/server/NewSpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.spring.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSpringAuthorizationServerApplication.class, args);
+    }
+
+}
