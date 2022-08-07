1e22ce40398ea66f531749ada9765d7a840c23a1
Art <d.art.shishkin@gmail.com>
2022-08-07 10:25:57 +0300

    Context Loads Test (#32)

---
+++ new-spring-authorization-server/src/test/java/net/shyshkin/study/oauth/spring/server/NewSpringAuthorizationServerApplicationTest.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.oauth.spring.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringAuthorizationServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
