1974fe43544edc46ddab113a7eb28de83c1a2705
Art <d.art.shishkin@gmail.com>
2022-08-07 12:05:44 +0300

    Get Authorization Code Test (#32)

--- new-spring-authorization-server/src/test/java/net/shyshkin/study/oauth/spring/server/NewSpringAuthorizationServerApplicationTest.java
+++ new-spring-authorization-server/src/test/java/net/shyshkin/study/oauth/spring/server/NewSpringAuthorizationServerApplicationTest.java
@@ -1,12 +1,139 @@
 package net.shyshkin.study.oauth.spring.server;
 
-import org.junit.jupiter.api.Test;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@SpringBootTest
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class NewSpringAuthorizationServerApplicationTest {
+
+    private static final String REDIRECT_URI = "http://127.0.0.1:8080/authorized";
+
+    private static final String AUTHORIZATION_REQUEST = UriComponentsBuilder
+            .fromPath("/oauth2/authorize")
+            .queryParam("response_type", "code")
+            .queryParam("client_id", "client1")
+            .queryParam("scope", "openid read")
+            .queryParam("state", "some-state")
+            .queryParam("redirect_uri", REDIRECT_URI)
+            .toUriString();
+    private static final String DEFAULT_USERNAME = "art";
+    private static final String CORRECT_PASSWORD = "art_pass";
+
+    @Autowired
+    private WebClient webClient;
+
+    private static String authorizationCode;
+
+    @BeforeEach
+    void setUp() {
+        this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
+        this.webClient.getOptions().setRedirectEnabled(true);
+        this.webClient.getCookieManager().clearCookies();    // log out
+    }
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    @Order(10)
+    public void whenLoginSuccessfulThenDisplayNotFoundError() throws IOException {
+        HtmlPage page = this.webClient.getPage("/");
+
+        assertLoginPage(page);
+
+        this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        WebResponse signInResponse = signIn(page, DEFAULT_USERNAME, CORRECT_PASSWORD).getWebResponse();
+        assertThat(signInResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());    // there is no "default" index page
+    }
+
+    @Test
+    @Order(20)
+    public void whenLoginFailsThenDisplayBadCredentials() throws IOException {
+        HtmlPage page = this.webClient.getPage("/");
+        HtmlPage loginErrorPage = signIn(page, DEFAULT_USERNAME, "wrong-password");
+
+        HtmlElement alert = loginErrorPage.querySelector("div[role=\"alert\"]");
+        assertThat(alert).isNotNull();
+        assertThat(alert.getTextContent()).isEqualTo("Bad credentials");
+    }
+
+    @Test
+    @Order(30)
+    public void whenNotLoggedInAndRequestingTokenThenRedirectsToLogin() throws IOException {
+        HtmlPage page = this.webClient.getPage(AUTHORIZATION_REQUEST);
+
+        assertLoginPage(page);
+    }
+
+    @Test
+    @Order(40)
+    public void whenLoggingInAndRequestingTokenThenRedirectsToClientApplication() throws IOException {
+        // Log in
+        this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        this.webClient.getOptions().setRedirectEnabled(false);
+        signIn(this.webClient.getPage("/login"), DEFAULT_USERNAME, CORRECT_PASSWORD);
+
+        // Request Authorization code
+        WebResponse response = this.webClient.getPage(AUTHORIZATION_REQUEST).getWebResponse();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.MOVED_PERMANENTLY.value());
+        String location = response.getResponseHeaderValue("location");
+        assertThat(location).startsWith(REDIRECT_URI);
+        assertThat(location).contains("code=");
+        log.debug("Location: {}", location);
+
+        authorizationCode = extractCode(location);
+
+        log.debug("Code: {}", authorizationCode);
+
+    }
+
+    private String extractCode(String location) {
+        int indexOfCode = location.indexOf("code=");
+        return location.substring(indexOfCode + 5).split("&")[0];
+    }
+
+    private static <P extends Page> P signIn(HtmlPage page, String username, String password) throws IOException {
+        HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
+        HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
+        HtmlButton signInButton = page.querySelector("button");
+
+        usernameInput.type(username);
+        passwordInput.type(password);
+        return signInButton.click();
+    }
+
+    private static void assertLoginPage(HtmlPage page) {
+        assertThat(page.getUrl().toString()).endsWith("/login");
+
+        HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
+        HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
+        HtmlButton signInButton = page.querySelector("button");
+
+        assertThat(usernameInput).isNotNull();
+        assertThat(passwordInput).isNotNull();
+        assertThat(signInButton.getTextContent()).isEqualTo("Sign in");
+    }
+
+
 }
