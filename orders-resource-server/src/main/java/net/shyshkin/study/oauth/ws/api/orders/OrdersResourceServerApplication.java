13879e8a57acd1a77911d2696726fc31762b5ceb
Art <d.art.shishkin@gmail.com>
2022-08-08 00:01:22 +0300

    177. Creating and Configure Resource Server (#33 Section 18: Resource Server for Spring Authorization Server)

---
+++ orders-resource-server/src/main/java/net/shyshkin/study/oauth/ws/api/orders/OrdersResourceServerApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.oauth.ws.api.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersResourceServerApplication.class, args);
+    }
+
+}
---
+++ orders-resource-server/src/test/java/net/shyshkin/study/oauth/ws/api/orders/OrdersResourceServerApplicationTest.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.oauth.ws.api.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersResourceServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
