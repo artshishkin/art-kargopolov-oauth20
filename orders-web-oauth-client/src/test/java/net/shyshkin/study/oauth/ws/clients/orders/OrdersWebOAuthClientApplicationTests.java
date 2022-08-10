604250bf96622299ebacdb6206af4ab86568910d
Art <d.art.shishkin@gmail.com>
2022-08-10 18:18:12 +0300

    186.3 Test Redirect to Login Page (#34)

--- orders-web-oauth-client/src/test/java/net/shyshkin/study/oauth/ws/clients/orders/OrdersWebOAuthClientApplicationTests.java
+++ orders-web-oauth-client/src/test/java/net/shyshkin/study/oauth/ws/clients/orders/OrdersWebOAuthClientApplicationTests.java
@@ -1,25 +1,53 @@
 package net.shyshkin.study.oauth.ws.clients.orders;
 
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Slf4j
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Testcontainers
 @ActiveProfiles("local")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource(properties = {
+        "server.port=8180"
+})
 class OrdersWebOAuthClientApplicationTests {
+
+    private static final String DEFAULT_USERNAME = "art";
+    private static final String CORRECT_PASSWORD = "art_pass";
+
+    private String webAppUri;
+
+    @LocalServerPort
+    int serverPort;
+
+    private WebClient webClient;
 
     private static final int hostPort = 8000;
     private static final int containerExposedPort = 8080;
@@ -29,11 +57,80 @@ class OrdersWebOAuthClientApplicationTests {
     static GenericContainer<?> newSpringAuthServer = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-new-spring-authorization-server")
             .withNetworkAliases("new-spring-auth-server")
             .withEnv("app.auth-server.provider.issuer", "http://localhost:8000")
+            .withEnv("app.client.baseUri", "http://127.0.0.1:8180")
             .withExposedPorts(8080)
             .withCreateContainerCmdModifier(cmd)
+            .withLogConsumer(new Slf4jLogConsumer(log))
             .waitingFor(Wait.forHealthcheck());
 
+    @BeforeEach
+    void setUp() throws IOException {
+        this.webClient = new WebClient();
+        this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
+        this.webClient.getOptions().setRedirectEnabled(true);
+        this.webClient.getCookieManager().clearCookies();    // log out
+
+        webAppUri = "http://127.0.0.1:" + serverPort;
+    }
+
     @Test
+    @Order(1)
     void contextLoads() {
     }
+
+    @Test
+    @Order(10)
+    public void whenNotLoggedInThenRedirectsToLogin() throws IOException {
+
+        HtmlPage page = this.webClient.getPage(webAppUri + "/orders");
+
+        assertLoginPage(page);
+    }
+
+    @Test
+    @Order(20)
+    public void whenLoginSuccessfulThenDisplayNotFoundError() throws IOException {
+        log.debug("WebAppUri: {}", webAppUri);
+        HtmlPage page = this.webClient.getPage(webAppUri + "/");
+
+        assertLoginPage(page);
+
+        this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        WebResponse signInResponse = signIn(page, DEFAULT_USERNAME, CORRECT_PASSWORD).getWebResponse();
+        assertThat(signInResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());    // there is no "default" index page
+    }
+
+    @Test
+    @Order(30)
+    public void whenLoginFailsThenDisplayBadCredentials() throws IOException {
+        HtmlPage page = this.webClient.getPage(webAppUri + "/");
+        HtmlPage loginErrorPage = signIn(page, DEFAULT_USERNAME, "wrong-password");
+
+        HtmlElement alert = loginErrorPage.querySelector("div[role=\"alert\"]");
+        assertThat(alert).isNotNull();
+        assertThat(alert.getTextContent()).isEqualTo("Bad credentials");
+    }
+
+    private static <P extends Page> P signIn(HtmlPage page, String username, String password) throws IOException {
+        HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
+        HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
+        HtmlButton signInButton = page.querySelector("button");
+
+        usernameInput.type(username);
+        passwordInput.type(password);
+        return signInButton.click();
+    }
+
+    private static void assertLoginPage(HtmlPage page) {
+        assertThat(page.getUrl().toString()).endsWith("/login");
+
+        HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
+        HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
+        HtmlButton signInButton = page.querySelector("button");
+
+        assertThat(usernameInput).isNotNull();
+        assertThat(passwordInput).isNotNull();
+        assertThat(signInButton.getTextContent()).isEqualTo("Sign in");
+    }
+
 }
