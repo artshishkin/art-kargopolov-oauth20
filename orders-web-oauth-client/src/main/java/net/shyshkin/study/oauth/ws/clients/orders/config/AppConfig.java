6e96bb88a982b24e8352bb6b2908b5d381a31069
Art <d.art.shishkin@gmail.com>
2022-08-10 10:52:18 +0300

    185. Sending HTTP Request (#34)

---
+++ orders-web-oauth-client/src/main/java/net/shyshkin/study/oauth/ws/clients/orders/config/AppConfig.java
@@ -0,0 +1,14 @@
+package net.shyshkin.study.oauth.ws.clients.orders.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- orders-web-oauth-client/src/main/java/net/shyshkin/study/oauth/ws/clients/orders/controller/OrdersController.java
+++ orders-web-oauth-client/src/main/java/net/shyshkin/study/oauth/ws/clients/orders/controller/OrdersController.java
@@ -1,29 +1,49 @@
 package net.shyshkin.study.oauth.ws.clients.orders.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.oauth.ws.clients.orders.dto.Order;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class OrdersController {
 
-    @GetMapping("/orders")
-    public String getAllOrders(Model model,
-                               @RegisteredOAuth2AuthorizedClient("orders-web-oauth-client") OAuth2AuthorizedClient authorizedClient) {
+    public static final ParameterizedTypeReference<List<Order>> ORDER_LIST_TYPE = new ParameterizedTypeReference<>() {
+    };
+    private final RestTemplate restTemplate;
 
-        List<Order> orders = new ArrayList<>();
-        model.addAttribute("orders", orders);
+    @Value("${app.services.orders.uri}")
+    private String ordersServiceUri;
+
+    @GetMapping("/orders")
+    public String getAllOrders(
+            Model model,
+            @RegisteredOAuth2AuthorizedClient("orders-web-oauth-client") OAuth2AuthorizedClient authorizedClient) {
 
         String jwtToken = authorizedClient.getAccessToken().getTokenValue();
         log.debug("JWT Token: {}", jwtToken);
+
+        RequestEntity<Void> requestEntity = RequestEntity.get(ordersServiceUri)
+                .headers(h -> h.setBearerAuth(jwtToken))
+                .build();
+
+        ResponseEntity<List<Order>> responseEntity = restTemplate.exchange(requestEntity, ORDER_LIST_TYPE);
+        List<Order> orders = responseEntity.getBody();
+
+        model.addAttribute("orders", orders);
 
         return "orders-page";
     }
