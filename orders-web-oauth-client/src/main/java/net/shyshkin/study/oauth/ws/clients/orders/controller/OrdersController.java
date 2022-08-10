3a7b3b0240eef0e9dafc09653e08fa0c40333018
Art <d.art.shishkin@gmail.com>
2022-08-10 10:37:52 +0300

    184. Reading the JWT Access Token (#34)

--- orders-web-oauth-client/src/main/java/net/shyshkin/study/oauth/ws/clients/orders/controller/OrdersController.java
+++ orders-web-oauth-client/src/main/java/net/shyshkin/study/oauth/ws/clients/orders/controller/OrdersController.java
@@ -1,6 +1,9 @@
 package net.shyshkin.study.oauth.ws.clients.orders.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.oauth.ws.clients.orders.dto.Order;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class OrdersController {
 
     @GetMapping("/orders")
-    public String getAllOrders(Model model) {
+    public String getAllOrders(Model model,
+                               @RegisteredOAuth2AuthorizedClient("orders-web-oauth-client") OAuth2AuthorizedClient authorizedClient) {
 
         List<Order> orders = new ArrayList<>();
         model.addAttribute("orders", orders);
+
+        String jwtToken = authorizedClient.getAccessToken().getTokenValue();
+        log.debug("JWT Token: {}", jwtToken);
 
         return "orders-page";
     }
