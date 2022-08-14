83477880569395f1787e4d39313da6fe7fd976ec
Art <d.art.shishkin@gmail.com>
2022-08-14 18:21:01 +0300

    52.3 Get Access Token via htmlunit WebClient (#52)

--- spa-javascript-client/src/test/java/net/shyshkin/study/oauth/clients/pkce/SpaJavascriptClientApplicationWebClientDockerTest.java
+++ spa-javascript-client/src/test/java/net/shyshkin/study/oauth/clients/pkce/SpaJavascriptClientApplicationWebClientDockerTest.java
@@ -1,38 +1,32 @@
 package net.shyshkin.study.oauth.clients.pkce;
 
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.DefaultJavaScriptErrorListener;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.oauth.test.containers.KeycloakStackContainers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Testcontainers
 @TestPropertySource(properties = {
         "logging.level.net.shyshkin=debug",
@@ -44,27 +38,12 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
     public static final String RESOURCE_OWNER_USERNAME = "shyshkin.art";
     public static final String RESOURCE_OWNER_PASSWORD = "password_art_1";
 
-    private static final String REDIRECT_URI = "http://127.0.0.1:8080/authorized";
-
-    private static final String AUTHORIZATION_REQUEST = UriComponentsBuilder
-            .fromPath("/oauth2/authorize")
-            .queryParam("response_type", "code")
-            .queryParam("client_id", "client1")
-            .queryParam("scope", "openid read")
-            .queryParam("state", "some-state")
-            .queryParam("redirect_uri", REDIRECT_URI)
-            .toUriString();
-
-    private static final String TOKEN_REQUEST_URL = "/oauth2/token";
-
-    private static final String CLIENT_ID = "client1";
-    private static final String CLIENT_PASSWORD = "myClientSecretValue";
+    @LocalServerPort
+    int serverPort;
 
     private WebClient webClient;
 
     private String baseUri;
-
-    RestTemplate restTemplate;
 
     @Container
     static KeycloakStackContainers keycloakStackContainers = KeycloakStackContainers.getInstance();
@@ -73,65 +52,75 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
 
     static Network network = keycloakStackContainers.getStackNetwork();
 
-    @Container
-    static GenericContainer<?> discoveryService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-discovery-service")
-            .withNetwork(network)
-            .withNetworkAliases("discovery-service")
-            .withExposedPorts(8080)
-            .waitingFor(Wait.forHealthcheck());
+//    @Container
+//    static GenericContainer<?> discoveryService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-discovery-service")
+//            .withNetwork(network)
+//            .withNetworkAliases("discovery-service")
+//            .withExposedPorts(8080)
+//            .waitingFor(Wait.forHealthcheck());
 
-    @Container
-    static GenericContainer<?> usersService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-resource-server")
-            .withNetwork(network)
-            .withNetworkAliases("users-service")
-            .withExposedPorts(8080)
-            .withEnv("eureka.client.enabled", "true")
-            .dependsOn(keycloak, discoveryService)
-            .waitingFor(Wait.forHealthcheck());
+//    @Container
+//    static GenericContainer<?> usersService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-resource-server")
+//            .withNetwork(network)
+//            .withNetworkAliases("users-service")
+//            .withExposedPorts(8080)
+//            .withEnv("eureka.client.enabled", "true")
+//            .dependsOn(keycloak, discoveryService)
+//            .waitingFor(Wait.forHealthcheck());
+//
+//    @Container
+//    static GenericContainer<?> gatewayService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-api-gateway")
+//            .withNetwork(network)
+//            .withNetworkAliases("gateway-service")
+//            .withExposedPorts(8080)
+//            .withEnv("eureka.client.enabled", "true")
+//            .withEnv("spring.profiles.active", "local")
+//            .withEnv("discovery.service.uri", "http://discovery-service:8080")
+//            .withEnv("eureka.client.registry-fetch-interval-seconds", "1")
+//            .dependsOn(usersService, discoveryService)
+//            .waitingFor(Wait.forHealthcheck());
 
-    @Container
-    static GenericContainer<?> gatewayService = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-api-gateway")
-            .withNetwork(network)
-            .withNetworkAliases("gateway-service")
-            .withExposedPorts(8080)
-            .withEnv("eureka.client.enabled", "true")
-            .withEnv("spring.profiles.active", "local")
-            .withEnv("discovery.service.uri", "http://discovery-service:8080")
-            .withEnv("eureka.client.registry-fetch-interval-seconds", "1")
-            .dependsOn(usersService, discoveryService)
-            .waitingFor(Wait.forHealthcheck());
 
-
-    @Container
-    static GenericContainer<?> spaJavascriptClient = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-spa-javascript-client")
-            .withNetwork(network)
-            .withNetworkAliases("spa-javascript-client")
-            .withEnv(Map.of(
-                    "logging.level.net.shyshkin", "debug",
-                    "app.redirect.host.uri", "http://spa-javascript-client:8080",
-                    "app.oauth.uri", "http://keycloak:8080",
-                    "app.gateway.uri", "http://gateway-service:8080",
-                    "app.users-api.uri", "http://users-service:8080"
-            ))
-            .withExposedPorts(8080)
-            .dependsOn(keycloak, gatewayService, usersService)
-            .withLogConsumer(new Slf4jLogConsumer(log))
-            .waitingFor(Wait.forHealthcheck());
+//    @Container
+//    static GenericContainer<?> spaJavascriptClient = new GenericContainer<>("artarkatesoft/art-kargopolov-oauth20-spa-javascript-client")
+//            .withNetwork(network)
+//            .withNetworkAliases("spa-javascript-client")
+//            .withEnv(Map.of(
+//                    "logging.level.net.shyshkin", "debug",
+//                    "app.redirect.host.uri", "http://spa-javascript-client:8080",
+//                    "app.oauth.uri", "http://keycloak:8080",
+//                    "app.gateway.uri", "http://gateway-service:8080",
+//                    "app.users-api.uri", "http://users-service:8080"
+//            ))
+//            .withExposedPorts(8080)
+//            .dependsOn(keycloak/*, gatewayService, usersService*/)
+//            .withLogConsumer(new Slf4jLogConsumer(log))
+//            .waitingFor(Wait.forHealthcheck());
 
 
     @BeforeEach
     void setUp() {
 
         this.webClient = new WebClient();
-        this.baseUri = "http://localhost:" + spaJavascriptClient.getMappedPort(8080);
+        this.baseUri = "http://localhost:" + serverPort;
         this.webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
         this.webClient.getOptions().setThrowExceptionOnScriptError(false);
         this.webClient.getOptions().setRedirectEnabled(true);
+        this.webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        this.webClient.setJavaScriptErrorListener(new DefaultJavaScriptErrorListener() {
+            @Override
+            public void scriptException(HtmlPage page, ScriptException scriptException) {
+                log.debug("JS error in line {} column {} Failing line\n`{}`\nscript source code {}",
+                        scriptException.getFailingLineNumber(),
+                        scriptException.getFailingColumnNumber(),
+                        scriptException.getFailingLine(),
+                        scriptException.getScriptSourceCode()
+                );
+                scriptException.printStackTrace();
+            }
+        });
         this.webClient.getCookieManager().clearCookies();    // log out
 
-        this.restTemplate = new RestTemplateBuilder()
-                .rootUri(baseUri)
-                .build();
     }
 
     @Test
@@ -142,12 +131,14 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
         //visit index page should show correct content
         HtmlPage page = webClient.getPage(indexPageUrl);
 
+        webClient.waitForBackgroundJavaScript(1000L);
+
         assertThat(page.getTitleText()).isEqualTo("Javascript Application with PKCE");
-        assertThat(page.getHtmlElementById("redirectHostUri").getTextContent()).isEqualTo("http://spa-javascript-client:8080");
-        assertThat(page.getHtmlElementById("oAuthServerUri").getTextContent()).isEqualTo("http://keycloak:8080");
-        assertThat(page.getHtmlElementById("usersApiUri").getTextContent()).isEqualTo("http://users-service:8080");
-        assertThat(page.getHtmlElementById("gatewayUri").getTextContent()).isEqualTo("http://gateway-service:8080");
-        assertThat(page.getHtmlElementById("resourceServerUri").getAttribute("value")).isEqualTo("http://users-service:8080");
+        assertThat(page.getHtmlElementById("redirectHostUri").getTextContent()).isEqualTo("http://localhost:8181");
+        assertThat(page.getHtmlElementById("oAuthServerUri").getTextContent()).isEqualTo(String.format("http://%s:%d", keycloak.getHost(), keycloak.getMappedPort(8080)));
+        assertThat(page.getHtmlElementById("usersApiUri").getTextContent()).isEqualTo("http://localhost:8666");
+        assertThat(page.getHtmlElementById("gatewayUri").getTextContent()).isEqualTo("http://localhost:8090");
+        assertThat(page.getHtmlElementById("resourceServerUri").getAttribute("value")).isEqualTo("http://localhost:8666");
 
         //click on button `Generate Random State Value` should change text in `stateValue` field
         page.getHtmlElementById("generateStateBtn").click();
@@ -157,29 +148,37 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
         page.getHtmlElementById("generateCodeVerifierBtn").click();
         assertThat(page.getHtmlElementById("codeVerifierValue").getTextContent()).isNotEqualTo("Code Verifier Value");
 
-//        logCurrentPage();
-
         //click on button `Generate Code Challenge Value` should change text in `codeVerifierValue` field
-        WebElement generateCodeChallengeBtn = page.getHtmlElementById("generateCodeChallengeBtn");
+        HtmlButton generateCodeChallengeBtn = page.getHtmlElementById("generateCodeChallengeBtn");
         generateCodeChallengeBtn.click();
         log.debug("generateCodeChallengeBtn: {}", generateCodeChallengeBtn);
 
-        page.getHtmlElementById("generateCodeChallengeBtn").click();
         assertThat(page.getHtmlElementById("codeChallengeValue").getTextContent()).isNotEqualTo("Code Challenge Value");
+
+        WebWindow currentWindow = webClient.getCurrentWindow();
 
         //click on button `Get Auth Code` should pop up new window for signing into keycloak
         page.getHtmlElementById("getAuthCodeBtn").click();
 
-        assertLoginPage(page);
+        List<WebWindow> webWindows = webClient.getWebWindows();
+        WebWindow loginWindow = webWindows.get(1);
+
+        webClient.waitForBackgroundJavaScript(3000L);
+
+        HtmlPage loginPage = (HtmlPage) loginWindow.getEnclosedPage();
+
+        assertLoginPage(loginPage);
 
         //correct signing in should redirect to index page with Access Token Generated
-        page = signIn(page, RESOURCE_OWNER_USERNAME, RESOURCE_OWNER_PASSWORD);
-
+        page = signIn(loginPage, RESOURCE_OWNER_USERNAME, RESOURCE_OWNER_PASSWORD);
 
         assertSpaPage(page);
-        assertThat(page.getHtmlElementById("accessToken").getTextContent())
+        String accessToken = page.getHtmlElementById("accessToken").getTextContent();
+        assertThat(accessToken)
                 .isNotBlank()
                 .hasSizeGreaterThan(20);
+
+        log.debug("Access Token: {}", accessToken);
 
         //click on buttons `Get Info From Resource Server directly` should show alert with "Working..." message
         List<String> directResourceServerButtonsIds = List.of(
@@ -249,7 +248,7 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
     private static <P extends Page> P signIn(HtmlPage page, String username, String password) throws IOException {
         HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
         HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
-        HtmlButton signInButton = page.querySelector("button");
+        HtmlInput signInButton = page.querySelector("input[name=\"login\"]");
 
         usernameInput.type(username);
         passwordInput.type(password);
@@ -257,21 +256,20 @@ class SpaJavascriptClientApplicationWebClientDockerTest {
     }
 
     private static void assertLoginPage(HtmlPage page) {
-        assertThat(page.getUrl().toString()).endsWith("/login");
+        assertThat(page.getUrl().toString()).contains("/auth/realms/katarinazart/protocol/openid-connect/auth");
 
         HtmlInput usernameInput = page.querySelector("input[name=\"username\"]");
         HtmlInput passwordInput = page.querySelector("input[name=\"password\"]");
-        HtmlButton signInButton = page.querySelector("button");
+        HtmlInput signInButton = page.querySelector("input[name=\"login\"]");
         String title = page.getTitleText();
 
         assertThat(title).isEqualTo("Sign in to katarinazart");
         assertThat(usernameInput).isNotNull();
         assertThat(passwordInput).isNotNull();
-        assertThat(signInButton.getTextContent()).isEqualTo("Sign in");
+        assertThat(signInButton.getValueAttribute()).isEqualToIgnoringCase("Sign In");
     }
 
     private static void assertSpaPage(HtmlPage page) {
-        assertThat(page.getUrl().toString()).endsWith("/login");
         String title = page.getTitleText();
         assertThat(title).isEqualTo("Javascript Application with PKCE");
     }
